e0adcbd76a78a946a3188a2dff3914967965fc0b
GuiFerreira01 <88511664+GuiFerreira01@users.noreply.github.com>
2021-09-03 16:15:35 -0300

    Add files via upload

--- _js/calcular_juros.js
+++ _js/calcular_juros.js
@@ -1,19 +1,21 @@
 
 function juros_composto() {
-    var nome, mensalidade, juros, anos;
+    var nome, mensalidade, juros, anos, i, n;
   
-    
+    i = 0.03
     nome = $("#nome").val();
     mensalidade = Number($("#Mensalidade").val());
-    juros = (mensalidade*((1+0.03)^(anos*12)-1))/0.03;
     anos = Number($("#anos").val());
-    arredondado = parseFloat(juros.toFixed(2));
+    juros = (mensalidade * (1+0.03) * ((Math.pow(1+0.03,(anos*12))-1)/0.03)).toFixed(2);
 
+    console.log(juros)
+    console.log(mensalidade)
+    console.log(anos)
 
     $(".col-4").addClass("d-none");
     $(".textinho").addClass("d-none");
     $(".col-8").removeClass("d-none");
-    $(".result-value").html(`<p> Óla ${nome}, juntando R$${mensalidade} todo mês, você terá R$${arredondado} em ${anos} anos com uma taxa de 3% </p>`);
+    $(".result-value").html(`<p> Óla ${nome}, juntando R$${mensalidade} todo mês, você terá R$${juros} em ${anos} anos com uma taxa de 3% </p>`);
 
 }
 
